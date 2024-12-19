0ced7ecde62a83699f6131ec36f1a6d9586b88fe
jeconia auma <jeconiaauma@gmail.com>
2024-12-19 14:14:09 +0300

    test on footer

--- partials/footer.php
+++ partials/footer.php
@@ -155,25 +155,25 @@
     All Js File
     ============================== -->
     <!-- Jquery -->
-    <script src="/assets/node_modules/jquery/dist/jquery.js"></script>
-    <script src="/assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="./assets/node_modules/jquery/dist/jquery.js"></script>
+    <script src="./assets/js/vendor/jquery-3.6.0.min.js"></script>
     <!-- Slick Slider -->
-    <script src="/assets/js/slick.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
     <!-- Bootstrap -->
-    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
     <!-- Magnific Popup -->
-    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.min.js"></script>
     <!-- Counter Up -->
-    <script src="/assets/js/jquery.counterup.min.js"></script>
+    <script src="./assets/js/jquery.counterup.min.js"></script>
     <!-- Range Slider -->
-    <script src="/assets/js/jquery-ui.min.js"></script>
+    <script src="./assets/js/jquery-ui.min.js"></script>
     
     <!-- Isotope Filter -->
-    <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
-    <script src="/assets/js/isotope.pkgd.min.js"></script>
+    <script src="./assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="./assets/js/isotope.pkgd.min.js"></script>
     
     <!-- Main Js File -->
-    <script src="/assets/js/main.js"></script>
+    <script src="./assets/js/main.js"></script>
 </body>
 
 </html>
